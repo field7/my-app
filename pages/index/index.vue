999b80812b973b56f43591c63ac1b11f9bb00a6e
庾荣钊 <field3@126.com>
2025-09-02 00:49:59 +0800

    页面提交

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -550,12 +550,12 @@ function findHandle(index) {
 	let index5 = arrRes.map(item => item).indexOf(min2)
 	arrRes[index4] = temp3
 	let objMaxMin = findMaxMin(arr)
-	if (index1 == objMaxMin.max || index1 == objMaxMin.min) {
+	if (index1 == index4) {
 		index1 = index2
 	}
-	if (index4 == objMaxMin.max || index4 == objMaxMin.min) {
-		index4 = index5
-	}
+	// if (index4 == objMaxMin.max || index4 == objMaxMin.min) {
+	// 	index4 = index5
+	// }
 	if (index == 0) {
 		if (arr[0] == oneMaxLast.value || arr[0] == oneMin.value) {
 			orderNumber1.value ++
